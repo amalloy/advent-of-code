bc38bbc87cc8c35af326ed9b0c871946656ebdb3
Alan Malloy <alan@malloys.org>
2015-12-17 15:45:55 -0800

    day8, part 2

--- day8/day8.hs
+++ day8/day8.hs
@@ -1,5 +1,5 @@
 import Control.Applicative
-
+import Control.Arrow
 
 stringOverhead :: String -> Int
 stringOverhead "" = 2
@@ -7,4 +7,7 @@ stringOverhead ('\\':'x':_:_:xs) = 3 + stringOverhead xs
 stringOverhead ('\\':_:xs) = 1 + stringOverhead xs
 stringOverhead (_:xs) = stringOverhead xs
 
-main = interact $ show . sum . map stringOverhead . lines
+stringInflation :: String -> Int
+stringInflation xs = 2 + (length $ filter (`elem` "\\\"") xs)
+
+main = interact $ show . (sum . map stringOverhead &&& sum . map stringInflation). lines
