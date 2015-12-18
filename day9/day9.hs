d3589dbe0c1365b8ff509af1a5f66e528138ea8c
Alan Malloy <alan@malloys.org>
2015-12-18 01:43:46 -0800

    day9: implement choosing from the available routes

--- day9/day9.hs
+++ day9/day9.hs
@@ -1,13 +1,26 @@
+import Data.Maybe
+
 type City = String
 type Distance = Int
 data Route = Route City City Distance deriving Show
+data Trip = Trip City Distance deriving Show
+data Progress = Progress City Distance [Route]
 
 parse :: String -> Route
 parse s = case words s of
   [a, "to", b, "=", dist] -> Route a b (read dist)
 
-destination :: City -> Route -> Maybe City
-destination c (Route a b dest)
-  | c == a = Just b
-  | c == b = Just a
+destination :: City -> Route -> Maybe Trip
+destination c (Route a b dist)
+  | c == a = go b
+  | c == b = go a
   | otherwise = Nothing
+  where go x = Just $ Trip x dist
+
+trips :: City -> [Route] -> [Trip]
+trips from = catMaybes . map (destination from)
+
+advance :: Progress -> [Progress]
+advance (Progress city dist routes) = do
+  (Trip to dist') <- trips city routes
+  return . Progress to (dist + dist') $ filter (isNothing . destination city) routes
