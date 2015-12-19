b4e03e8babf87911e6a8641c50d5320ffe618b40
Alan Malloy <alan@malloys.org>
2015-12-18 17:56:25 -0800

    day12, part 2

--- day12/src.hs
+++ day12/src.hs
@@ -1,13 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Data.Aeson
 import qualified Data.ByteString.Lazy as B
 import Data.HashMap.Strict (elems)
 import Data.Scientific (toBoundedInteger)
 import Data.Maybe (maybeToList, fromJust)
+import Data.Text (Text)
+import Control.Arrow
 
-nums :: Value -> [Int]
-nums (Object o) = concatMap nums (elems o)
-nums (Array a) = concatMap nums a
-nums (Number n) = maybeToList . toBoundedInteger $ n
-nums _ = []
+nums :: (Text -> Bool) -> Value -> [Int]
+nums pred = go where
+  pred' (String s) = pred s
+  pred' _ = False
+  go (Object o) = if any pred' (elems o) then [] else concatMap go (elems o)
+  go (Array a) = concatMap go a
+  go (Number n) = maybeToList . toBoundedInteger $ n
+  go _ = []
 
-main = (print . sum . nums . fromJust . decode) =<< B.getContents
+main = (print . (sum . (nums (const False))
+                 &&&
+                 (sum . (nums (== "red"))))
+        . fromJust . decode)
+       =<< B.getContents
