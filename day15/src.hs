1494c69dc81cbaf2c98f09d3f4611a40df79a6a2
Alan Malloy <alan@malloys.org>
2015-12-19 14:21:50 -0800

    day15: parser, and a function to produce all solutions

--- day15/src.hs
+++ day15/src.hs
@@ -8,7 +8,8 @@ type Stat = String -- flavor, etc
 data Factor = Factor Stat Int
 type Impact = [Factor]
 type Recipe = M.Map Ingredient Int
-type Bakery = Reader (M.Map Ingredient Impact)
+type Cookbook = (M.Map Ingredient Impact)
+type Bakery = Reader Cookbook
 
 updateIn :: Ord k => M.Map k v -> k -> (v -> v -> v) -> v -> M.Map k v
 updateIn m k f v = M.unionWith f m $ M.singleton k v
@@ -19,3 +20,22 @@ bake = foldM mix M.empty . M.toList where
     impact <- asks (M.! ingredient)
     return $ foldr add m impact
     where add (Factor stat i) m = updateIn m stat (+) (i * amount)
+
+parse :: String -> (Ingredient, Impact)
+parse s = (init ingr, go more) where
+  (ingr:more) = words s
+  go [k,v] = [Factor k (read v)]
+  go (k:v:more) = Factor k (read (init v)) : go more
+
+parseAll :: String -> Cookbook
+parseAll = M.fromList . map parse . lines
+
+mixes :: Ord a => Int -> [a] -> [M.Map a Int]
+mixes 0 xs = [M.fromList [(x, 0) | x <- xs]]
+mixes n [] = []
+mixes n (x:xs) = do
+  xAmt <- [1..n]
+  rest <- mixes (n - xAmt) xs
+  return $ M.insert x xAmt rest
+
+main = interact $ show . parseAll
