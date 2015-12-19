4b209246615083dbd612915ae88f4c0621815698
Alan Malloy <alan@malloys.org>
2015-12-19 02:34:20 -0800

    Refactoring for part2
    - Give each reindeer a point tracker
    - Get ready to run the race one second at a time instead of all at once

--- day14/src.hs
+++ day14/src.hs
@@ -7,28 +7,32 @@ type Name = String
 
 -- reindeer descriptors
 data State = Resting Time | Running Time deriving Show
-data Position = Position {dist :: Distance, state :: State} deriving Show
+data Position = Position {dist :: Distance, points :: Int, state :: State} deriving Show
 data Stats = Stats {speed :: Distance, burst :: Time, rest :: Time} deriving Show
-data Reindeer = Reindeer {name :: Name, stats :: Stats, position :: Position} deriving Show
+data Reindeer = Reindeer {stats :: Stats, position :: Position} deriving Show
 
 start :: Position
-start = Position 0 (Resting 0)
+start = Position 0 0 (Resting 0)
 
 parse :: String -> Reindeer
-parse s = Reindeer n (Stats (read v) (read b - 1) (read r - 1)) start
-  where [n, _, _, v, _, _, b, _, _, _, _, _, _, r, _] = words s
+parse s = Reindeer (Stats (read v) (read b - 1) (read r - 1)) start
+  where [_, _, _, v, _, _, b, _, _, _, _, _, _, r, _] = words s
 
-run :: Stats -> Position -> Position
-run s (Position d (Resting 0)) = Position (d + speed s) (Running (burst s))
-run s (Position d (Resting n)) = Position d (Resting (n - 1))
-run s (Position d (Running 0)) = Position d (Resting (rest s))
-run s (Position d (Running n)) = Position (d + speed s) (Running (n - 1))
+rDist :: Reindeer -> Distance
+rDist = dist . position
 
-runFor :: Time -> Reindeer -> Reindeer
-runFor n r = r {position = newPos}
-  where newPos = iterate (run (stats r)) (position r) !! n
+addPoints :: Int -> Reindeer -> Reindeer
+addPoints n r@(Reindeer {position = pos}) = r {position = pos {points = n + points pos}}
+
+run :: Stats -> Reindeer -> Reindeer
+run s r = r {position = pos (position r)}
+  where pos (Position d p state) = case state of
+          (Resting 0) -> Position (d + speed s) p (Running (burst s))
+          (Resting n) -> Position d p (Resting (n - 1))
+          (Running 0) -> Position d p (Resting (rest s))
+          (Running n) -> Position (d + speed s) p (Running (n - 1))
 
 race :: Time -> [Reindeer] -> [Reindeer]
-race secs = map (runFor secs)
+race secs = undefined
 
-main = interact $ show . maximum . map (dist . position) . race 2503 . map parse . lines
+-- main = interact $ show . maximum . map (dist . position) . race 2503 . map parse . lines
