d2f1f98949ad02c40b051a73fd87bcc31d1a7d37
Alan Malloy <alan@malloys.org>
2015-12-20 15:51:01 -0800

    day18: parser, life8-specific rules

--- day18/src.hs
+++ day18/src.hs
@@ -1,4 +1,5 @@
 import qualified Data.Map as M
+import Control.Applicative
 import Data.Maybe (mapMaybe)
 
 data Life coord light = Life {neighbors :: (coord -> [coord]),
@@ -13,5 +14,28 @@ advance game = game {grid = M.fromList . map tick . M.toList $ m}
                                  others = mapMaybe (`M.lookup` m) coords
                              in next game light others)
 
-type Coord a = (a,a)
-type Light = Bool
+grid8 :: Num a => [(a,a)]
+grid8 = let ds = [0,-1,1]
+        in tail $ liftA2 (,) ds ds
+
+applyDelta :: Num a => (a,a) -> (a,a) -> (a,a)
+applyDelta (x,y) (x',y') = (x+x',y+y')
+
+neighbors8 :: Num a => (a,a) -> [(a,a)] -> [(a,a)]
+neighbors8 delta = fmap (applyDelta delta)
+
+nextBool :: Bool -> [Bool] -> Bool
+nextBool True others = length (filter id others) `elem` [2,3]
+nextBool False others = length (filter id others) == 3
+
+rows :: String -> [[(Int, Bool)]]
+rows s = zipWith mkLight [0..] <$> lines s
+
+mkLight :: Int -> Char -> (Int, Bool)
+mkLight x char = (x, char == '#')
+
+mkRow :: Int -> [(Int, Bool)] -> [((Int, Int), Bool)]
+mkRow y lights = [((y,x),on) | (x,on) <- lights]
+
+parse :: String -> M.Map (Int,Int) Bool
+parse = M.fromList . concat . zipWith mkRow [0..] . rows
