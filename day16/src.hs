4c2d47ba3a606b232a52936c5724c337b9c14aa6
Alan Malloy <alan@malloys.org>
2015-12-19 21:38:50 -0800

    day16, part1

--- day16/src.hs
+++ day16/src.hs
@@ -8,16 +8,22 @@ slurp :: FilePath -> IO [String]
 slurp p = lines <$> (hGetContents =<< openFile p ReadMode)
 
 parseGoal :: [String] -> Info
-parseGoal = undefined
+parseGoal = M.fromList . map parsePiece
+  where parsePiece s = (init k, read v)
+          where [k, v] = words s
 
-parse :: String -> Info
-parse = undefined
+parse :: String -> (Int, Info)
+parse s = (read (init n), M.fromList (entries more))
+  where ("Sue":n:more) = words s
+        entries [k, v] = [(init k, read v)]
+        entries (k:v:more) = (init k, read (init v)) : entries more
 
 satisfies :: Info -> Info -> Bool
-satisfies goal obj = undefined
+satisfies goal obj = all match (M.toList obj)
+  where match (k, v) = goal M.! k == v
 
 main = do
   [goalFile, searchFile] <- getArgs
   goal <- parseGoal <$> slurp goalFile
   sues <- map parse <$> slurp searchFile
-  print . head . filter (satisfies goal) $ sues
+  print . fst . head . filter (satisfies goal . snd) $ sues
