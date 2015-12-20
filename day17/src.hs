28478799bd3d144924642732457330bcc5c02bcf
Alan Malloy <alan@malloys.org>
2015-12-20 00:44:06 -0800

    day17: part 1 done. Easy if you read the rules right.

--- day17/src.hs
+++ day17/src.hs
@@ -1,25 +1,15 @@
-import qualified Data.Map as M
 import Control.Monad
 
 type Volume = Int
 type Container = Int
-type Pantry = M.Map Container Int
-
-updateIn :: Ord k => M.Map k v -> k -> (v -> v -> v) -> v -> M.Map k v
-updateIn m k f v = M.unionWith f m $ M.singleton k v
-
-frequencies :: Ord a => [a] -> M.Map a Int
-frequencies = foldr add M.empty where
-  add x m = updateIn m x (+) 1
+type Pantry = [Container]
 
 readPantry :: String -> Pantry
-readPantry = frequencies . map read . lines
+readPantry = map read . lines
 
 distribute :: Volume -> Pantry -> [Pantry]
 distribute 0 p = [p]
-distribute n p = do
-  (size, count) <- M.toList p
-  guard $ count > 0 && size <= n
-  distribute (n - size) (M.adjust (subtract 1) size p)
+distribute n [] = []
+distribute n (c:p) = distribute n p ++ ((c:) <$> distribute (n-c) p)
 
 main = interact $ show . length . distribute 150 . readPantry
