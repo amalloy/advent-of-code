d63470798d2516daea97cb0696c78774a47bdd7f
Alan Malloy <alan@malloys.org>
2015-12-20 22:25:49 -0800

    day19, part1

--- day19/src.hs
+++ day19/src.hs
@@ -1,9 +1,12 @@
 import qualified Data.Map as M
-import Data.Char
+import Data.Char (isUpper)
+import Data.List (nub)
 
 type Atom = String
 type Molecule = [Atom]
 data Transition a = Transition a [a] deriving Show
+type Grammar a = M.Map a [[a]]
+data Problem a = Problem (Grammar a) [a] deriving Show
 
 parseAtom :: String -> (Atom, String)
 parseAtom [x] = ([x], "")
@@ -15,8 +18,31 @@ parseMolecule [] = []
 parseMolecule s = let (a, more) = parseAtom s
                   in a : parseMolecule more
 
-parseTransition :: String -> Transition String
+parseTransition :: String -> Transition Atom
 parseTransition s = let [from, _, to] = words s
                         (a, "") = parseAtom from
                         m = parseMolecule to
                     in Transition a m
+
+parse :: String -> Problem Atom
+parse s = let strs = filter (not . null) $ lines s
+              rules = map parseTransition $ init strs
+              goal = parseMolecule $ last strs
+              grammar = foldr addRule M.empty rules
+          in Problem grammar goal
+  where addRule (Transition from to) m =
+          M.insertWith (++) from [to] m
+
+expansions :: Ord a => Grammar a -> [a] -> [[a]]
+expansions m [] = []
+expansions m (x:xs) = leaveAlone ++ expandHere where
+  leaveAlone = (x:) <$> expansions m xs
+  expandHere = (++ xs) <$> M.findWithDefault [] x m
+
+expand :: Problem Atom -> [Molecule]
+expand (Problem grammar goal) = expansions grammar $ goal
+
+solve :: Problem Atom -> Int
+solve = length . nub . expand
+
+main = interact $ show . solve . parse
