0fdbdb4269162b86bc81b1df681dded943c88794
Alan Malloy <alan@malloys.org>
2015-12-23 06:21:33 -0800

    Add main to day22

--- day22/Day22/Main.hs
+++ day22/Day22/Main.hs
@@ -2,6 +2,7 @@ module Day22.Main where
 
 import Day22.Game
 import Day22.Search
+import Data.Maybe (fromJust)
 
 initPlayer :: Player
 initPlayer = Player 50 500
@@ -11,3 +12,6 @@ initBoss = Boss 51 9
 
 initCombat :: Combat
 initCombat = Combat PlayerTurn initPlayer initBoss []
+
+main = do
+  print . fst . fromJust . shortestPath $ Problem initCombat nexts outcome
