3ce35e566a5e0d7120fd0d7c96264c9ab2c60ccc
Alan Malloy <alan@malloys.org>
2016-08-24 10:40:43 -0700

    Add a more reasonable definition

--- day10/src.hs
+++ day10/src.hs
@@ -6,6 +6,9 @@ import Control.Arrow
 
 lookAndSay :: [Int] -> [Int]
 lookAndSay = sequenceA [length, head] <=< group
+-- the above silly/terse definition was just an exercise. a clearer definition would be:
+-- lookAndSay xs = group xs >>= describe
+--   where describe x = [length x, head x]
 
 expand :: [Int] -> [[Int]]
 expand = iterate lookAndSay
